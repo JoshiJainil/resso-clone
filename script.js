b7893d27a3ccb13a114a5270edca21f57b55003a
JoshiJainil <12002040701072@mbit.edu.in>
2022-09-07 19:01:40 +0530

    initialize basic code

---
+++ script.js
@@ -0,0 +1,112 @@
+console.log("welcome to Resso");
+
+
+let songIndex = 0;
+let audioElement = new Audio('songs/Electric.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+
+let songs = [
+    {songName: "Holy-justin beiber remix-trapcity", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "stileto-cravin", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "super villian-trapcity", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Believer-Nightcore", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "adem jensen-THE Highsociety -mystic", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"}, 
+    {songName: "adem jensen-mystic(remix)dyt ", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Blank-and- white-fascading", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Whispers=Ramssey", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Real Love-ajax ncx", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+]
+
+songItems.forEach((element, i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
+
+//Handleplay/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle'); 
+        gif.style.opacity = 1;   
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+//audioElement.play();
+
+//listen to event
+audioElement.addEventListener('timeupdate',()=>{
+    // Update Seekbar  
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+
+    }
+)
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const  makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=8){
+        songIndex = 0
+    }
+    else{
+        songIndex =songIndex+ 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    // masterSongName.innerText = songs[songIndex].songName;
+    
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
